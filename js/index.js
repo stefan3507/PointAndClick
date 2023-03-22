1940e2197ce7dd9d9aad381e86dee6277dfe3d4a
stefan3507 <s.van.vliet@sintlucasedu.nl>
2023-03-22 10:54:33 +0100

    1

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
 document.getElementById("mainTitle").appendChild(document.createTextNode("hello world"));
 document.getElementById("mainTitle").style.textAlign = "center";
+
+const mainCharacter = document.getElementById("MainCharacter")
+const gameWindow = document.getElementById("gameWindow")
+const offsetCharacter = 11;
+
+
+gameWindow.onclick = function(e) {
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+    mainCharacter.style.left = x - offsetCharacter + "px";
+    mainCharacter.style.top = y - offsetCharacter + "px";
+    //console.log(x);
+}
