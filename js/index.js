8310f82dba5d2f61da2131a96bea042535d5a751
stefan3507 <s.van.vliet@sintlucasedu.nl>
2023-04-05 12:34:56 +0200

    1

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@ const gameWindow = document.getElementById("gameWindow");
 
 const sec = 1000;
 
+let tempTimeOut;
+
 //Main character
 const mainCharacter = document.getElementById("mainCharacter");
 const characterAudio = document.getElementById("characterAudio");
@@ -16,72 +18,151 @@ const counterAudio = document.getElementById("counterAudio");
 const counterPortrait = document.getElementById("counterCharacter");
 
 //inventory
-const inventory = [];
+let inventory = [];
 const inventoryList = document.getElementById("inventoryList");
 
+function tweededeel () {
+    //setTimeout(function(){document.getElementById("tilemapimg").style.display = "none"}, 6* sec);
+    //setTimeout(function(){document.getElementById("tilemapimg2").style.opacity = 1}, 6* sec);
+    //setTimeout(function(){document.getElementById("door1").style.display = "none"}, 6* sec);
+    //setTimeout(function(){document.getElementById("oldhouse").style.display = "none"}, 6* sec);
+    //setTimeout(function(){document.getElementById("wisetree").style.display = "none"}, 6* sec);
+    //setTimeout(function(){document.getElementById("pond").style.display = "none"}, 6* sec);
+    //setTimeout(function(){document.getElementById("water1").style.display = "none"}, 6* sec);
+    //setTimeout(function(){document.getElementById("water2").style.display = "none"}, 6* sec);
+    //setTimeout(function(){document.getElementById("water3").style.display = "none"}, 6* sec);
+}
+
 gameWindow.onclick = function (e) {
+    if (counterSpeech.style.opacity == 0 && mainCharacterSpeech.style.opacity == 0) {
+        var rect = gameWindow.getBoundingClientRect();
+        var x = e.clientX - rect.left; //x position within the element.
+        var y = e.clientY - rect.top;  //y position within the element.
+        let currentX = mainCharacter.style.left;
+        let currentY = mainCharacter.style.top;
+        mainCharacter.style.left = x - offsetCharacter + "px";
+        mainCharacter.style.top = y - offsetCharacter + "px";
 
-    var rect = gameWindow.getBoundingClientRect();
-    var x = e.clientX - rect.left; //x position within the element.
-    var y = e.clientY - rect.top;  //y position within the element.
-    mainCharacter.style.left = x - offsetCharacter + "px";
-    mainCharacter.style.top = y - offsetCharacter + "px";
+        console.log(e.target.id);
 
-    console.log(e.target.id);
+        switch (e.target.id) {
+            case "door1":
+                if (checkItem("old key")) {
+                    showMessage(mainCharacterSpeech, characterAudio, "This key fits the door is open");
+                    showMessage(mainCharacterSpeech, characterAudio, "wow there is a secret tunnel here. I wonder what it leads to");
+                    tweededeel();
+                } else {
+                    showMessage(mainCharacterSpeech, characterAudio, "this door ia locked <br> maybe you need some sorth of key");
 
-    switch (e.target.id) {
-        case "house2":
-            //something insert here
-            if (checkItem("rusty key")) {
-                showMessage(mainCharacterSpeech, characterAudio, "This key fits the door is open<br> wow there is a secret tunnel maybe it will take me to that other island..");
-            } else {
-                showMessage(mainCharacterSpeech, characterAudio, "the door is locked");
+                }
+                break;
+            case "oldhouse":
+                if (!checkItem("fishing rope")) {
+                    getItem("fishing rope");
+                    showMessage(mainCharacterSpeech, characterAudio, "Wow there is a fishing rope in this house wonder what i can use this for");
+                    setTimeout(hideMessage, 4 * sec, mainCharacterSpeech, characterAudio);
+                } else {
+                    showMessage(mainCharacterSpeech, characterAudio, "This house is empty ...");
+                    setTimeout(hideMessage, 4 * sec, mainCharacterSpeech, characterAudio);
+                }
+                break;
+            case "signToLeft":
+                showMessage(mainCharacterSpeech, characterAudio, "Okay that house on the left<br>that's the house of the town wizard.");
+                break;
+            case "wisetree":
+                counterPortrait.style.opacity = 1;
+                showMessage(mainCharacterSpeech, characterAudio, "What is this tree why does it look so weird");
+                setTimeout(showMessage, 4 * sec, counterSpeech, counterAudio, "Finaly someone to talk to");
+                setTimeout(showMessage, 8 * sec, mainCharacterSpeech, characterAudio, "What do you mean? Trees are not supose to talk");
+                setTimeout(showMessage, 12 * sec, counterSpeech, counterAudio, "To escape this place you should get to that boat on the other island..");
+                setTimeout(function () { counterPortrait.style.opacity = 0; }, 16 * sec);
+                setTimeout(showMessage, 16 * sec, mainCharacterSpeech, characterAudio, "Wait! What?");
+                break;
+            case "pond":
+                if (checkItem("fishing rope")) {
+                    getItem("old key");
+                    showMessage(mainCharacterSpeech, characterAudio, "Wow I found a old key!<br>I wonder how long it has been in this pond");
+                    setTimeout(hideMessage, 4 * sec, mainCharacterSpeech, characterAudio);
+                } else {
+                    showMessage(mainCharacterSpeech, characterAudio, "I may need a fishing rope for this pond");
+                    setTimeout(hideMessage, 4 * sec, mainCharacterSpeech, characterAudio);
+                }
+                break;
 
-            }
-            break;
-        case "door2":
-            //something insert here
-            showMessage(mainCharacterSpeech, characterAudio, "noboby is home...<br> Come back later..");
-            break;
-        case "grave":
-            //something insert here
-            showMessage(mainCharacterSpeech, characterAudio, "cool house");
-            break;
-        case "signToLeft":
-            //something insert here
-            showMessage(mainCharacterSpeech, characterAudio, "Okay that house on the left<br>that's the house of the town wizard.");
-            break;
-        case "statue":
-            counterPortrait.style.opacity = 1;
-            showMessage(mainCharacterSpeech, characterAudio, "What is this for statue?");
-            setTimeout(showMessage, 4 * sec, counterSpeech, counterAudio, "Finaly someone to talk to");
-            setTimeout(showMessage, 8 * sec, mainCharacterSpeech, characterAudio, "What do you mean? Statues are not supose to talk anyway..");
-            setTimeout(showMessage, 12 * sec, counterSpeech, counterAudio, "You should check one off the graves");
-            setTimeout(function () { counterPortrait.style.opacity = 0; }, 16 * sec);
-            setTimeout(showMessage, 16 * sec, mainCharacterSpeech, characterAudio, "Wait! What?");
-            break;
-        case "tree":
-            if (!checkItem("rusty key")) {
-                getItem("rusty key");
-                showMessage(mainCharacterSpeech, characterAudio, "Wow I found a key!<br>I wonder what i can do with it..");
-                setTimeout(hideMessage, 4 * sec, mainCharacterSpeech, characterAudio);
-            } else {
-                showMessage(mainCharacterSpeech, characterAudio, "Nope nothing here...");
-                setTimeout(hideMessage, 4 * sec, mainCharacterSpeech, characterAudio);
-            }
+            case "water1":
+                showMessage(mainCharacterSpeech, characterAudio, "i dont know how to swim... <br> there must be a different way to get to that island")
+                mainCharacter.style.left = currentX;
+                mainCharacter.style.top = currentY;
+                break;
+            case "water2":
+                showMessage(mainCharacterSpeech, characterAudio, "i dont know how to swim... <br> there must be a different way to get to that island")
+                mainCharacter.style.left = currentX;
+                mainCharacter.style.top = currentY;
+                break;
+            case "water3":
+                showMessage(mainCharacterSpeech, characterAudio, "i dont know how to swim... <br> there must be a different way to get to that island")
+                mainCharacter.style.left = currentX;
+                mainCharacter.style.top = currentY;
+                break;
+            case "water4":
+                showMessage(mainCharacterSpeech, characterAudio, "i dont know how to swim... <br> and i probaly dont need that island anymore")
+                mainCharacter.style.left = currentX;
+                mainCharacter.style.top = currentY;
+                break;
+            case "water5":
+                showMessage(mainCharacterSpeech, characterAudio, "i dont know how to swim... <br> and i probaly dont need that island anymore")
+                mainCharacter.style.left = currentX;
+                mainCharacter.style.top = currentY;
+                break;
+            case "house3":
+                showMessage(mainCharacterSpeech, characterAudio, "Cool i found another key but what do i use this for..")
+                if (!checkItem("key")) {
+                    getItem("key");
+                    showMessage(mainCharacterSpeech, characterAudio, "Cool i found another key but what do i use this for..")
+                    setTimeout(hideMessage, 4 * sec, mainCharacterSpeech, characterAudio);
+                } else {
+                    showMessage(mainCharacterSpeech, characterAudio, "This house is empty ...");
+                    setTimeout(hideMessage, 4 * sec, mainCharacterSpeech, characterAudio);
+                }
+                break;
 
-            break;
-        default:
-            // do something when it doesn't have a case
-            hideMessage(mainCharacterSpeech, characterAudio);
-            hideMessage(counterSpeech, counterAudio);
-            break;
+            case "house4":
+                showMessage(mainCharacterSpeech, characterAudio, "This house has nothing in it");
+                break;
+
+            case "house5":
+                if (checkItem("key")) {
+                    getItem("wrench");
+                    showMessage(mainCharacterSpeech, characterAudio, "Wow this house has a wrench maybe i can repair the boat with this..");
+                    setTimeout(hideMessage, 4 * sec, mainCharacterSpeech, characterAudio);
+                } else {
+                    showMessage(mainCharacterSpeech, characterAudio, "This house is locked ...");
+                    setTimeout(hideMessage, 4 * sec, mainCharacterSpeech, characterAudio);
+                }
+                break;
+
+            case "house5":
+                showMessage(mainCharacterSpeech, characterAudio, "This is the house i just came from");
+                break;
+
+            case "boat":
+                if(checkItem("wrench")) {
+                    showMessage(mainCharacterSpeech, characterAudio, "Woohoo i repaired the boat");  
+                } else {
+                    showMessage(mainCharacterSpeech, characterAudio, "oh no this boat is broken");
+                }
+                break;
+            default:
+                // do something when it doesn't have a case
+                hideMessage(mainCharacterSpeech, characterAudio);
+                hideMessage(counterSpeech, counterAudio);
+                break;
+        }
     }
-
-    //console.log(x);
 }
 
 function showMessage(targetBalloon, targetSound, message) {
+    targetSound.currentTime = 0;
     targetSound.play();
     targetBalloon.style.opacity = "1";
     targetBalloon.innerHTML = message;
@@ -94,10 +175,10 @@ function hideMessage(targetBalloon, targetSound) {
     targetBalloon.innerHTML = "...";
 }
 
-function getItem(item) {
-    if (!checkItem(item)) {
-        inventory.push(item);
-        showItem(item);
+function getItem(itemName, itemId) {
+    if (!checkItem(itemName)) {
+        inventory.push(itemName);
+        showItem(itemName, itemId);
     }
 
 }
@@ -106,22 +187,26 @@ function checkItem(item) {
     return inventory.includes(item);
 }
 
-function showItem(toDoValue) {
+function showItem(itemName, itemId) {
     //Make a list item from scratch and store it in a variable
     let listItem = document.createElement("li");
 
+    //Give List Item an ID name
+    listItem.id = itemId;
+
     //fill that list item with value of inputfield
-    listItem.appendChild(document.createTextNode(toDoValue));
+    listItem.appendChild(document.createTextNode(itemName));
 
     //find UL with id todoContainer and attach the list item to it.
     inventoryList.appendChild(listItem);
 }
-/*
-function removeItem(item) {
-    inventory = inventory.filter(function(newInventory){
-        return newInventory !== item;
+
+function removeItem(itemName, itemId) {
+    //remove item in Array
+    inventory = inventory.filter(function (newInventory) {
+        return newInventory !== itemName;
     });
+    //removes list element in HTML
+    document.getElementById(itemId).remove();
+
 }
-getItem("rusty key");
-removeItem("rusty key");
-*/
